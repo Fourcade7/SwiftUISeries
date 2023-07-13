3ff2f0c5fd16818f088a8113e391aff150aeb061
fourcade7 <Fourcadeaminov7@gmail.com>
2023-07-13 23:57:35 +0500

    Initial Commit

---
+++ SwiftUISimple/SwiftUISimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISimpleApp.swift
+//  SwiftUISimple
+//
+//  Created by Fourcade7 on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

91978163858a1372bd887ed4ae7ab9d74502a939
fourcade7 <Fourcadeaminov7@gmail.com>
2023-07-16 03:31:31 +0500

    Alhamdulillah gg

--- SwiftUISimple/ContentView.swift
+++ SwiftUISimple/ContentView.swift
@@ -9,18 +9,116 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+        VStack(
+            alignment: .center) {
             Text("Hello, world!")
-        }
-        .padding()
+            Divider()
+            Text("Hello, world!")
+                    .padding(20)
+            Text("Hello")
+            HStack{
+                    Text("Hello, world!")
+                            .padding(20)
+                    Text("Hello")
+                    Text("Hello")
+            }
+                    
+            }.padding(10)
+            .frame(width: 200, height: 400)
+            .background(Color.red)
+            
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        TextFieldContentView()
     }
 }
+
+
+struct ContentView2: View {
+    var body: some View {
+        VStack(
+            alignment: .leading) {
+                Image("android")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(height: 250)
+            }.frame(height: .infinity)
+            
+            
+        
+    }
+}
+
+
+struct ContentView3: View {
+    @State var  changetoggle = true
+    var body: some View {
+        VStack(
+            alignment: .center) {
+                Toggle(isOn: $changetoggle, label:{
+                    Text("axaxax")
+                } ).padding(45)
+                
+                if changetoggle{
+                    Image("android")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(height: 250)
+                }
+                
+            }.frame(height: .infinity)
+        
+            
+            
+        
+    }
+}
+
+struct ButtonContentView: View {
+    @State var  changetoggle = true
+    var body: some View {
+        VStack(
+            alignment: .center) {
+                Button(action: {
+                    changetoggle = !changetoggle
+                }, label: {
+                    Text("Click me")
+                })
+                Toggle(isOn: $changetoggle, label:{
+                    Text("axaxax")
+                } ).padding(45)
+                
+            }.frame(height: .infinity)
+        
+            
+            
+        
+    }
+}
+
+struct TextFieldContentView: View {
+    @State var  name = ""
+    var body: some View {
+        VStack(
+            alignment: .center) {
+                TextField("enter your name", text: $name)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+               
+                
+            }.frame(height: .infinity)
+        
+            
+            
+        
+    }
+}
+
+
+
